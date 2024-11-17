c87fd1608909cc25d2eeece96a7036ad86fa6307
111Makss111 <777docmax777@gmail.com>
2024-11-17 15:21:27 +0100

    ю

--- js/task-6.js
+++ js/task-6.js
@@ -5,28 +5,29 @@ function getRandomHexColor() {
 }
 
 function createBoxes(amount) {
-  divBoxes.innerHTML = '';
+  divBoxes.innerHTML = ''; 
+  const fragment = document.createDocumentFragment(); 
+
   let size = 30;
   for (let i = 0; i < amount; i++) {
     const newBox = document.createElement('div');
-    newBox.classList.add('box');
     newBox.style.width = `${size}px`;
     newBox.style.height = `${size}px`;
     newBox.style.backgroundColor = getRandomHexColor();
-    divBoxes.appendChild(newBox);
+    fragment.appendChild(newBox); 
     size += 10;
   }
-};
 
+  divBoxes.appendChild(fragment); 
+}
 
 const inputElement = document.querySelector('input[type="number"]');
 const divBoxes = document.querySelector('#boxes');
 const clickCreate = document.querySelector('[data-create]');
 const clickDestroy = document.querySelector('[data-destroy]');
 
-
 clickCreate.addEventListener('click', () => {
-  let amount = parseInt(inputElement.value);
+  const amount = parseInt(inputElement.value);
   if (amount >= 1 && amount <= 100) {
     createBoxes(amount);
     inputElement.value = '';
@@ -34,9 +35,8 @@ clickCreate.addEventListener('click', () => {
 });
 
 clickDestroy.addEventListener('click', () => {
-  divBoxes.innerHTML = '';
+  divBoxes.innerHTML = ''; 
 });
-
 
 
 
